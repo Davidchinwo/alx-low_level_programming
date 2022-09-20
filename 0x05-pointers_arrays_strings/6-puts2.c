84b00d29f31d4f33f3d6abea69a59ca86ae9abf0
Davidchinwo <goodoodave@gmail.com>
2022-09-20 11:11:02 -0700

    .

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,43 +1,21 @@
 #include "main.h"
-
 /**
- * cap_string - function with one argument
- * @str: char type pointer
+ * puts2 - prints every other character of a string
+ * @str: the string to print
  *
- * Description: capitalizes all words of a string
- * Return: pointer
  */
-
-char *cap_string(char *str)
+void puts2(char *str)
 {
 	int i;
+	int j;
 
-	i = 0;
-	while (str[i] != '\0')
+	for (i = 0 ; str[i] != '\0' ; i++)
 	{
-		if (str[0] >= 'a' && str[0] <= 'z')
-			str[i] = str[i] - 32;
-		switch (str[i])
-		{
-		case ' ':
-		case '\t':
-		case '\n':
-		case ',':
-		case ';':
-		case '.':
-		case '!':
-		case '?':
-		case '"':
-		case '(':
-		case ')':
-		case '{':
-		case '}':
-			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
-			{
-				str[i + 1] = str[i + 1] - 32;
-			}
-		}
-		i++;
+		;
 	}
-	return (str);
+	for (j = 0 ; j < i ; j = j + 2)
+	{
+		_putchar(str[j]);
+	}
+	_putchar('\n');
 }
