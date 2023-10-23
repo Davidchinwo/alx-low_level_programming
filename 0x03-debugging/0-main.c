c437a380c8a256f2df1e819611de63cfb73c42f4
Davidchinwo <goodoodave@gmail.com>
2023-10-22 19:14:42 -0700

    som files

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -7,18 +7,10 @@
 
 int main(void)
 {
-	int i;
+int i;
 
-	i = 0;
-	positive_or_negative(i);
+i = 0;
+positive_or_negative(i);
 
-	return (0);
-#ifndef MAIN_H
-#define MAIN_H
-
-void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-int convert_day(int month, int day);
-void print_remaining_days(int month, int day, int year);
-
-#endif}
+return (0);
+}
